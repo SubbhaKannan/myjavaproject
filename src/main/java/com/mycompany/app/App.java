3ac99f423c74c45b36346780a8365a3a61b001a7
SubbhaKannan <120653566+SubbhaKannan@users.noreply.github.com>
2022-12-28 07:35:52 +0530

    updated for demo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Good morning!" );
+        System.out.println( "Good morning! have a nice day" );
     }
 }
